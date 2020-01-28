cee62735e83fd07b880ec1674a61cf0bb4607cbb
ThomasTW935 <thomastw935@gmail.com>
2020-01-28 23:50:25 +0800

    Student Navigation Desktop Mode Done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
-let navBurger = document.querySelector(".nav__Burger");
+let navBurger = document.querySelector(".nav__Burger")
 navBurger.addEventListener("click", () => {
-   let navList = document.querySelector(".nav__List");
+   let navList = document.querySelector(".nav__List")
+   let navBurger_Line = document.querySelector(".nav__Burger--Line")
    navList.classList.toggle("open")
+   navBurgetr_Line.classList.toggle("lineColor")
 })
