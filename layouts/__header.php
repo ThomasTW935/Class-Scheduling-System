0a90ff540510496afb5dee69c80b7bfb4883fd2b
ThomasTW935 <thomastw935@gmail.com>
2020-01-30 19:04:38 +0800

    Student View Navigation

--- layouts/__header.php
+++ layouts/__header.php
@@ -11,17 +11,17 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <link rel="stylesheet" href="styles/style.css"/>
+   <link rel="stylesheet" href="styles/index.css"/>
    <script defer src="scripts/script.js"></script>
    <title>Class Scheduling System</title>
 </head>
 <body>
-   <nav class="navScheduling">
-      <ul class="navScheduling__List">
-         <li class="navScheduling__List--Item"><a href="sections">Sections</a></li>
-         <li class="navScheduling__List--Item"><a href="room">Room</a></li>
-         <li class="navScheduling__List--Item"><a href="professors">Professors</a></li>
-         <li class="navScheduling__List--Item"><a href="subjects">Subjects</a></li>
+   <nav class="nav">
+      <ul class="nav__List">
+         <li class="nav__List--Item"><a href="index.php?sections">Sections</a></li>
+         <li class="nav__List--Item"><a href="index.php?room">Room</a></li>
+         <li class="nav__List--Item"><a href="index.php?professors">Professors</a></li>
+         <li class="nav__List--Item"><a href="index.php?subjects">Subjects</a></li>
       </ul>
       <form action="./includes/logout.inc.php" method="POST">
          <button type="submit" name="logout-Button" >Logout</button>
