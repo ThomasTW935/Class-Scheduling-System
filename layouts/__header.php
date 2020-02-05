2852dbaeac3810eeec3d4cbdefd42d8b3335d63e
ThomasTW935 <thomastw935@gmail.com>
2020-02-06 02:19:11 +0800

    Files Renaming

--- layouts/__header.php
+++ layouts/__header.php
@@ -11,7 +11,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <link rel="stylesheet" href="styles/index.css"/>
+   <link rel="stylesheet" href="./styles/admin.css"/>
    <script defer src="scripts/script.js"></script>
    <title>Class Scheduling System</title>
 </head>
