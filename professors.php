62b4f09efe54f9fdc69fb1bd74d2edd4d8b7c819
ThomasTW935 <thomastw935@gmail.com>
2020-02-05 11:24:22 +0800

    Professors Deisng 1.0

--- professors.php
+++ professors.php
@@ -11,14 +11,23 @@
       
       <main class="professors">
          <?php
+
+            echo '<ul class="professors__List professors__Title">';
+            foreach($professors as $key => $value){
+               echo '<li>'.$key.'</li>';
+            }
+            echo '<li></li>';
+            echo '</ul>';
             for($i = 0; $i<sizeof($professors['firstName']);$i++){
                echo '<ul class="professors__List">
-                  <li>'.$professors['firstName'][$i].'</li>
-                  <li>'.$professors['lastName'][$i].'</li>
-                  <li>'.$professors['deptName'][$i].'</li>
+                  <li class="professors__Item">'.$professors['firstName'][$i].'</li>
+                  <li class="professors__Item">'.$professors['lastName'][$i].'</li>
+                  <li class="professors__Item">'.$professors['deptName'][$i].'</li>
+                  <a href="?update&id='.$i.'" class="">Update</a>
                </ul>';
             }
-         ?>
+            ?>
+            <a href="?add" class="professors__Add form__Redirect">+</a>
       </main>
       
 <?php
