1fd1e0c21b46c5ee951280710ce3851156887b7b
ThomasTW935 <thomastw935@gmail.com>
2020-02-10 02:27:39 +0800

    Database Connection

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -1,5 +1,15 @@
 <?php
 
-class NewClass {
-   
+class Dbh {
+   private $host = "localhost";
+   private $user = "root";
+   private $pwd = "";
+   private $dbName = "ClassSchedulingSystem";
+
+   protected function connect(){
+      $dsn = 'mysql:host='. $this->$host .';dbname='. $this->$dbName;
+      $pdo = new PDO($dsn, $this->user, $this->$pwd);
+      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+      return $pdo;
+   }
 }
