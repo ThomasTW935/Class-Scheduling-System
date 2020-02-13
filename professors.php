8fdcfef4d28196f920e073a6bf8f092a0ea91155
ThomasTW935 <thomastw935@gmail.com>
2020-02-13 10:15:50 +0800

    Professors List Design 1.0

--- professors.php
+++ professors.php
@@ -9,45 +9,67 @@
 
 ?>
       
-      <main class="professors">
+      <main class='professors'>
+         <a href='?add' class='professors__Add form__Redirect'>+</a>
+         <div class='professors__Container'>
+            <ul class='professors__List professors__Title'>
+               <li class='professors__Item'>Employee ID</li>
+               <li class='professors__Item'>Employee Name</li>
+               <li class='professors__Item'>Department</li>
+               <li class='professors__Item'>CheckSchedule</li>
+               <li class='professors__Item'>Edit</li>
+               <li class='professors__Item'>Remove</li>
+            </ul>
+            <ul class='professors__List'>
+               <li class='professors__Item'>064000</li>
+               <li class='professors__Item'>Thomas, Daryl P.</li>
+               <li class='professors__Item'>BS-IT</li>
+               <li class='professors__Item'>CheckSchedule</li>
+               <li class='professors__Item'>Edit</li>
+               <li class='professors__Item'>Remove</li>
+            </ul>
+         </div>
          <?php
 
-            echo '<ul class="professors__List professors__Title">';
-            foreach($professors as $key => $value){
-               echo '<li>'.$key.'</li>';
-            }
-            echo '<li></li>';
-            echo '</ul>';
-            for($i = 0; $i<sizeof($professors['firstName']);$i++){
-               echo '<ul class="professors__List">
-                  <li class="professors__Item">'.$professors['firstName'][$i].'</li>
-                  <li class="professors__Item">'.$professors['lastName'][$i].'</li>
-                  <li class="professors__Item">'.$professors['deptName'][$i].'</li>
-                  <a href="?update&id='.$i.'" class="">Update</a>
-               </ul>';
+            // echo "<ul class='professors__List professors__Title'>";
+            // foreach($professors as $key => $value){
+            //    echo "<li>".$key."</li>";
+            // }
+            // echo '<li></li>';
+            // echo '</ul>';
+            // for($i = 0; $i<sizeof($professors['firstName']);$i++){
+            //    echo "<ul class='professors__List'>
+            //       <li class='professors__Item'>".$professors['firstName'][$i]."</li>
+            //       <li class='professors__Item'>".$professors['lastName'][$i]."</li>
+            //       <li class='professors__Item'>".$professors['deptName'][$i]."</li>
+            //       <a href='?update&id='.$i.'' class=''>Update</a>
+            //    </ul>";
+            // }
+
+            if(isset($_GET['add'])){
+               echo " 
+               <form action='./includes/professors.inc.php' class='professors__Form' method='POST'>
+                  <label for='formSelect' class='form__Title'>Professor's Information</label>
+                  <input class='form__Input' type='text' name='employeeID' placeholder='Employee ID'>
+                  <input class='form__Input' type='text' name='lastName' placeholder='Last Name'>
+                  <input class='form__Input' type='text' name='firstName' placeholder='First Name'>
+                  <input class='form__Input' type='text' name='middleInitial' placeholder='M.I'>
+                  <input class='form__Input' type='text' name='suffix' placeholder='Suffix'>
+                  <div>
+                     <label for='formSelect'>Department:</label>
+                     <select class='form__Select' name='deptName' id='formSelect'>
+                        <option class='form__Option' value='1'>BSIT</option>
+                        <option class='form__Option' value='2'>BSCS</option>
+                        <option class='form__Option' value='3'>BSCE</option>
+                        <option class='form__Option' value='4'>BSAT</option>
+                     </select>
+                  </div>
+                  <button class='form__Button' type='submit' name='submit'>Submit</button>
+               </form>
+               <div class='form__Background'></div>";
             }
 
             ?>
-            <a href="?add" class="professors__Add form__Redirect">+</a>
-            <form action="./includes/professors.inc.php" class="professors__Form" method="POST">
-               <label for="formSelect" class="form__Title">Professor's Information</label>
-               <input class="form__Input" type="text" name="employeeID" placeholder="Employee ID">
-               <input class="form__Input" type="text" name="lastName" placeholder="Last Name">
-               <input class="form__Input" type="text" name="firstName" placeholder="First Name">
-               <input class="form__Input" type="text" name="middleInitial" placeholder="M.I">
-               <input class="form__Input" type="text" name="suffix" placeholder="Suffix">
-               <div>
-                  <label for="formSelect">Department:</label>
-                  <select class="form__Select" name="deptName" id="formSelect">
-                     <option class="form__Option" value="1">BSIT</option>
-                     <option class="form__Option" value="2">BSCS</option>
-                     <option class="form__Option" value="3">BSCE</option>
-                     <option class="form__Option" value="4">BSAT</option>
-                  </select>
-               </div>
-               <button class="form__Button" type="submit" name="submit">Submit</button>
-            </form>
-            <div class="form__Background"></div>
       </main>
       
 <?php
