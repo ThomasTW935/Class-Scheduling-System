59c169d22f6aad0c15e6881882a573d5f21edce1
ThomasTW935 <thomastw935@gmail.com>
2020-02-13 20:02:40 +0800

    kahit ano

--- dashboard.php
+++ dashboard.php
@@ -4,28 +4,36 @@
       
 <main class="dashboard">
    <a class="dashboard__Card card__Design" href="./professors.php">
-      <h1>Professors</h1>
-      <img src="./drawables/icons/professor-default.svg" alt="Professors">
+      <h1>PROFESSORS</h1>
+      <img src="./drawables/icons/professor.svg" alt="Professors">
+   </a>
+   <a class="dashboard__Card card__Design" href="#">
+      <h1>STUDENTS</h1>
+      <img src="./drawables/icons/student.svg" alt="Professors">
    </a>
    <a class="dashboard__Card card__Design" href="./rooms.php">
-      <h1>Rooms</h1>
-      <img src="./drawables/icons/professor-default.svg" alt="Professors">
-   </a>
-   <a class="dashboard__Card card__Design" href="#">
-      <h1>Course</h1>
-      <img src="./drawables/icons/professor-default.svg" alt="Professors">
+      <h1>ROOMS</h1>
+      <img src="./drawables/icons/rooms.svg" alt="Professors">
    </a>
    <a class="dashboard__Card card__Design" href="./departments.php">
-      <h1>Departments</h1>
-      <img src="./drawables/icons/professor-default.svg" alt="Professors">
+      <h1>TERTIARY COURSES</h1>
+      <img src="./drawables/icons/tertiary.svg" alt="Professors">
    </a>
    <a class="dashboard__Card card__Design" href="#">
-      <h1>Course</h1>
-      <img src="./drawables/icons/professor-default.svg" alt="Professors">
+      <h1>SHS STRAND</h1>
+      <img src="./drawables/icons/shs.svg" alt="Professors">
    </a>
    <a class="dashboard__Card card__Design" href="#">
-      <h1>Course</h1>
-      <img src="./drawables/icons/professor-default.svg" alt="Professors">
+      <h1>STUDENT DEPARTMENT</h1>
+      <img src="./drawables/icons/studentdepartment.svg" alt="Professors">
+   </a>
+   <a class="dashboard__Card card__Design" href="#">
+      <h1>FACULTY DEPARTMENT</h1>
+      <img src="./drawables/icons/faculty.svg" alt="Professors">
+   </a>
+   <a class="dashboard__Card card__Design" href="#">
+      <h1>SECTIONS</h1>
+      <img src="./drawables/icons/section.svg" alt="SECTIONS">
    </a>
 </main>
       
