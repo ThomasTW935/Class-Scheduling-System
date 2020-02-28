ecf4334619432d11f3c58fe86f37c5767d3450e4
ThomasTW935 <thomastw935@gmail.com>
2020-02-28 17:14:00 +0800

    Test 2

--- professors.php
+++ professors.php
@@ -24,9 +24,45 @@
          </div>
          <?php
             //if(isset($_GET['add']) || isset($_GET['id'])){
-               include_once './layouts/professors.form.php';
+               //include_once './layouts/professors.form.php';
             //}
          ?>
+         <form action='./includes/professors.inc.php' class='module__Form' method='POST' enctype="multipart/form-data">
+   <a href="professors.php" class='form__Close'>X</a>
+   <label for='formSelect' class='form__Title'>Professor's Information</label>
+   <input class='form__Input' type='hidden' value='<?php echo $prof['id'] ?? '' ?>' name='id'>
+   <input id="empID" class='form__Input' type='text' value='<?php echo $prof['emp_no'] ?? '' ?>' name='employeeID' placeholder='Employee ID' maxlength="8" required>
+   <div class="form__Error"><?php echo $errors['employeeID'] ?? '' ?></div>
+   <input class='form__Input' type='text' value='<?php echo $prof['last_name'] ?? '' ?>' name='lastName' placeholder='Last Name' required>
+   <div class="form__Error"><?php echo $errors['lastName'] ?? '' ?></div>
+   <input class='form__Input' type='text' value='<?php echo $prof['first_name'] ?? '' ?>' name='firstName' placeholder='First Name' required>
+   <div class="form__Error"><?php echo $errors['firstName'] ?? '' ?></div>
+   <input class='form__Input' type='text' value='<?php echo $prof['middle_initial'] ?? '' ?>' name='middleInitial' placeholder='M.I' >
+   <div class="form__Error"><?php echo $errors['middleInitial'] ?? '' ?></div>
+   <input class='form__Input' type='text' value='<?php echo $prof['suffix'] ?? '' ?>' name='suffix' placeholder='Suffix'>
+   <div class="form__Error"><?php echo $errors['suffix'] ?? '' ?></div>
+   <input id="userName" class='form__Input' type='text' value='<?php echo $prof['username'] ?? '' ?>' name='username' placeholder='Username'>
+   <div class="form__Error"><?php echo $errors['username'] ?? '' ?></div>
+   
+   <div>
+      <label for='formSelect'>Department:</label>
+      <select class='form__Select' name='deptID' id='formSelect'>
+         <?php
+            $deptView = new DepartmentsView();
+            $departments = $deptView->FetchDepts("Faculty");
+            foreach($departments as $dept){
+               $selected = ($prof['dept_id'] == $dept['dept_id']) ? 'selected' : '';
+               echo "<option class='form__Option' value='". $dept['dept_id'] ."' ". $selected .">". $dept['dept_name'] ."</option>";
+            }
+
+         ?>
+      </select>
+   </div>
+   <input class='form__Input' type='file' value='<?php echo $prof['username'] ?? '' ?>' name='image' accept="image/*">
+   <div class="form__Error"><?php echo $errors['image'] ?? '' ?></div>
+   <button class='form__Button' type='submit' name='<?php echo $button ?>'><?php echo $button ?></button>
+</form>
+<a href="professors.php" class='module__formBackground'></a>
    
       </main>
       
