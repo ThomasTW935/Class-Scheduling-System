63a7096aae0d1dd4cd76e0955da52787204201d6
ThomasTW935 <thomastw935@gmail.com>
2020-02-28 17:23:34 +0800

    Test

--- login.php
+++ login.php
@@ -20,6 +20,23 @@
    <script defer src="./scripts/login.js"></script>
 </head>
 <body>   
-   
+   <form class="form" action="includes/login.inc.php" method="POST">
+      <div class="form__Message">
+         <label class="form__Label">Sign in</label>
+         <p class="form__Signin--display">as <span></span></p>
+      </div>
+      <div class="form__Groups">
+         <div class="form__Group form__Signin--hide">
+            <input type="text"  class="form__Field" placeholder="Username" id='username' name="username" <?php echo $display ?>>
+            <label for='username' class="form__Label--placeholder">Username</label>
+         </div>
+         <div class="form__Group form__Signin--display">
+            <input type="password" class="form__Field form__Signin--display" placeholder="Password" id='password'  name="password" autofocus>
+            <label for="password" class="form__Label--placeholder form__Signin--display">Password</label>
+         </div>
+         <span class="form__Error"></span>
+      </div>
+      <button class="form__Button" type="submit" name="<?php echo $buttonName ?>">NEXT</button>
+   </form>
 </body>
 </html>
