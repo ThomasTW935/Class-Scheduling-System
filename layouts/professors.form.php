021c4403caebc75f8916b6b73d7374872c7540be
ThomasTW935 <thomastw935@gmail.com>
2020-08-04 13:42:27 +0800

    Professors Form Error Messages Fixeed

--- layouts/professors.form.php
+++ layouts/professors.form.php
@@ -4,32 +4,51 @@ $url = $_SERVER['REQUEST_URI'];
 $query = parse_url($url, PHP_URL_QUERY);
 $button = "submit";
 parse_str($query, $errors);
+$empID = (!empty($errors['empID'])) ? $errors['empID'] : '';
+$firstName = (!empty($errors['firstName'])) ? $errors['firstName'] : '';
+$lastName = (!empty($errors['lastName'])) ? $errors['lastName'] : '';
+$middleName = (!empty($errors['middleInitial'])) ? $errors['middleInitial'] : '';
+$suffix = (!empty($errors['suffix'])) ? $errors['suffix'] : '';
+$username = (!empty($errors['username'])) ? $errors['username'] : '';
+$email = (!empty($errors['email'])) ? $errors['email'] : '';
+
+$errorEmpID       = (!empty($errors['errorEmpID'])) ? $errors['errorEmpID'] : '';
+$errorFirstName   = (!empty($errors['errorFirstname'])) ? $errors['errorFirstname'] : '';
+$errorLastName    = (!empty($errors['errorLastname'])) ? $errors['errorLastname'] : '';
+$errorMiddleName  = (!empty($errors['errorMiddlename'])) ? $errors['errorMiddlename'] : '';
+$errorSuffix      = (!empty($errors['errorSuffix'])) ? $errors['errorSuffix'] : '';
+$errorUsername    = (!empty($errors['errorUsername'])) ? $errors['errorUsername'] : '';
+$errorEmail       = (!empty($errors['errorEmail'])) ? $errors['errorEmail'] : '';
 if (isset($_GET['id']) && !empty($_GET['id'])) {
    $id = $_GET['id'];
    $profView = new ProfessorsView();
    $result = $profView->FetchProfessorByID($id);
    $prof = $result[0];
+   $prodID = $prof['id'];
+   $userID = $prof['user_id'];
    $button = "update";
+   $empID =  $prof['emp_no'];
+   $firstName =  $prof['first_name'];
+   $lastName =  $prof['last_name'];
+   $middleName = $prof['middleInitial'];
+   $suffix =  $prof['suffix'];
+   $username =  $prof['username'];
+   $email =  $prof['email'];
 }
-$empID = (!empty($_GET['empID'])) ? $_GET['empID'] : $prof['emp_no'];
-$firstName = (!empty($_GET['firstName'])) ? $_GET['firstName'] : $prof['first_name'];
-$lastName = (!empty($_GET['lastName'])) ? $_GET['lastName'] : $prof['last_name'];
-$middleName = (!empty($_GET['middleInitial'])) ? $_GET['middleInitial'] : '';
-$suffix = (!empty($_GET['suffix'])) ? $_GET['suffix'] : $prof['suffix'];
-$username = (!empty($_GET['username'])) ? $_GET['username'] : $prof['username'];
-$email = (!empty($_GET['email'])) ? $_GET['email'] : $prof['email'];
 ?>
 
 <form action='./includes/professors.inc.php' class='module__Form' method='POST' enctype="multipart/form-data">
-   <a href="professors.php" class='form__Close'>X</a>
+   <section class='form__Close'>
+      <a href="professors.php">X</a>
+   </section>
    <label class='form__Title'>Professor's Information</label>
-   <input type='hidden' value='<?php echo $prof['id'] ?? '' ?>' name='profID'>
-   <input type='hidden' value='<?php echo $prof['user_id'] ?? '' ?>' name='userID'>
+   <input type='hidden' value='<?php echo $profID ?? '' ?>' name='profID'>
+   <input type='hidden' value='<?php echo $userID ?? '' ?>' name='userID'>
    <div class="form__Container">
       <label for='' class='form__Label'>Employee ID:</label>
       <div class="form__Input">
          <input id="empID" type='text' value='<?php echo $empID ?? '' ?>' name='empID' required>
-         <div class="form__Error"><?php echo $errors['errorEmpID'] ?? '' ?></div>
+         <div class="form__Error"><?php echo $errorEmpID ?></div>
       </div>
    </div>
    <div class="form__Container">
@@ -39,10 +58,10 @@ $email = (!empty($_GET['email'])) ? $_GET['email'] : $prof['email'];
          <input class='name' type='text' value='<?php echo $lastName ?? '' ?>' name='lastName' placeholder='Last Name' required>
          <input class='name--short' type='text' value='<?php echo $middleName ?? '' ?>' name='middleInitial' placeholder='M.I'>
          <input class='name--short' type='text' value='<?php echo $suffix ?? '' ?>' name='suffix' placeholder='Suffix'>
-         <div class="form__Error"><?php echo $errors['errorFirstname'] ?? '' ?></div>
-         <div class="form__Error"><?php echo $errors['errorLastname'] ?? '' ?></div>
-         <div class="form__Error"><?php echo $errors['errorMiddlename'] ?? '' ?></div>
-         <div class="form__Error"><?php echo $errors['errorSuffix'] ?? '' ?></div>
+         <div class="form__Error"><?php echo $errorFirstName ?></div>
+         <div class="form__Error"><?php echo $errorLastName ?></div>
+         <div class="form__Error"><?php echo $errorMiddleName ?></div>
+         <div class="form__Error"><?php echo $errorSuffix ?></div>
          <div></div>
       </div>
    </div>
@@ -50,14 +69,14 @@ $email = (!empty($_GET['email'])) ? $_GET['email'] : $prof['email'];
       <label for='' class='form__Label'>Username:</label>
       <div class="form__Input">
          <input id="userName" type='text' value='<?php echo $username ?? '' ?>' name='username' required>
-         <div class="form__Error"><?php echo $errors['errorUserame'] ?? '' ?></div>
+         <div class="form__Error"><?php echo $errorUsername ?></div>
       </div>
    </div>
    <div class="form__Container">
       <label class='form__Label'>Email:</label>
       <div class="form__Input">
          <input type='text' value='<?php echo $email ?? '' ?>' name='email'>
-         <div class="form__Error"><?php echo $errors['errorEmail'] ?? '' ?></div>
+         <div class="form__Error"><?php echo $errorEmail ?></div>
       </div>
    </div>
    <div class="form__Container">
