d317985300604c0f0308a14acecd269f41206eb1
ThomasTW935 <thomastw935@gmail.com>
2020-08-06 03:16:12 +0800

    Added DB Config for Heroku

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -2,10 +2,11 @@
 
 class Dbh
 {
-   private $host = "localhost";
-   private $user = "root";
-   private $pwd = "";
-   private $dbName = "ClassSchedulingSystem";
+
+   private $host = 'us-cdbr-east-02.cleardb.com';
+   private $user = "b9e547a06bfec3";
+   private $pwd = "0e937125";
+   private $dbName = "heroku_47a94c0a6cb5111";
 
    protected function connect()
    {
