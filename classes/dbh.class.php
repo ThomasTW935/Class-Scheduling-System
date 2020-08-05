233a99b8f1fcc48f5ce077af7f73c17f87719102
ThomasTW935 <thomastw935@gmail.com>
2020-08-06 04:24:20 +0800

    Heroku Test 6

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -15,12 +15,13 @@ class Dbh
    protected function connect()
    {
       try {
-         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+         $dsn = "mysql:host=" . $this->$host . "; dbname=" . $this->$dbname;
          $pdo = new PDO($dsn, $this->user, $this->pwd);
          $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
          return $pdo;
       } catch (PDOException $e) {
-         trigger_error("Error: " . $e->getMessage());
+         print "Error!: " . $e->getMessage() . "<br/>";
+         die();
       }
    }
    private function sql()
