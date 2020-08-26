f008009f271712c2200b649fb46dda3a535c46b6
ThomasTW935 <thomastw935@gmail.com>
2020-08-26 12:35:23 +0800

    Update Heroku

--- classes/Dbh.class.php
+++ classes/Dbh.class.php
@@ -3,14 +3,14 @@
 class Dbh
 {
 
-   private $host = 'localhost';
-   private $user = "root";
-   private $pwd = "";
-   private $dbName = "ClassSchedulingSystem";
-   // private $host = 'us-cdbr-east-02.cleardb.com';
-   // private $user = "b9e547a06bfec3";
-   // private $pwd = "0e937125";
-   // private $dbName = "heroku_47a94c0a6cb5111";
+   // private $host = 'localhost';
+   // private $user = "root";
+   // private $pwd = "";
+   // private $dbName = "ClassSchedulingSystem";
+   private $host = 'us-cdbr-east-02.cleardb.com';
+   private $user = "b9e547a06bfec3";
+   private $pwd = "0e937125";
+   private $dbName = "heroku_47a94c0a6cb5111";
    // $herokuHost = mysql -u b9e547a06bfec3 -h us-cdbr-east-02.cleardb.com -p heroku_47a94c0a6cb5111 < classschedulingsystem.sql
 
    // mysql://b9e547a06bfec3:0e937125@us-cdbr-east-02.cleardb.com/heroku_47a94c0a6cb5111?reconnect=true
