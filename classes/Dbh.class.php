21005073cf0734738e8215906b1db2489794d3ff
ThomasTW935 <thomastw935@gmail.com>
2020-09-19 19:54:20 +0800

    Update: Heroku

--- classes/Dbh.class.php
+++ classes/Dbh.class.php
@@ -3,15 +3,15 @@
 class Dbh
 {
 
-   private $host = 'localhost';
-   private $user = "root";
-   private $pwd = "";
-   private $dbName = "ClassSchedulingSystem";
-   // private $host = 'us-cdbr-east-02.cleardb.com';
-   // private $user = "b9e547a06bfec3";
-   // private $pwd = "0e937125";
-   // private $dbName = "heroku_47a94c0a6cb5111";
-   // private $test = ';';
+   // private $host = 'localhost';
+   // private $user = "root";
+   // private $pwd = "";
+   // private $dbName = "ClassSchedulingSystem";
+   private $host = 'us-cdbr-east-02.cleardb.com';
+   private $user = "b9e547a06bfec3";
+   private $pwd = "0e937125";
+   private $dbName = "heroku_47a94c0a6cb5111";
+   private $test = ';';
    // $herokuHost = mysql -u b9e547a06bfec3 -h us-cdbr-east-02.cleardb.com -p heroku_47a94c0a6cb5111 < classschedulingsystem.sql
 
    protected function connect()
