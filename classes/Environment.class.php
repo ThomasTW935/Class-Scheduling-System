632b28f2a3d38316abcdcf3ccceddef95f149774
ThomasTW935 <thomastw935@gmail.com>
2020-09-25 11:34:04 +0800

    Update: Environment

--- classes/Environment.class.php
+++ classes/Environment.class.php
@@ -9,7 +9,6 @@ class Environment
   private $pwd = "";
   private $dbName = "ClassSchedulingSystem";
 
-  // $herokuHost = mysql -u b9e547a06bfec3 -h us-cdbr-east-02.cleardb.com -p heroku_47a94c0a6cb5111 < classschedulingsystem.sql
 
   protected function getHost()
   {
