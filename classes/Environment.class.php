e9370f5ddc88435d7e7bcfeb28649aecd7bd8073
ThomasTW935 <thomastw935@gmail.com>
2020-09-25 11:33:05 +0800

    Update: Database

--- classes/Environment.class.php
+++ classes/Environment.class.php
@@ -2,15 +2,12 @@
 
 class Environment
 {
-  private $host = 'us-cdbr-east-02.cleardb.com';
-  private $user = "b9e547a06bfec3";
-  private $pwd = "0e937125";
-  private $dbName = "heroku_47a94c0a6cb5111";
 
-  // private $host = 'localhost';
-  // private $user = "root";
-  // private $pwd = "";
-  // private $dbName = "ClassSchedulingSystem";
+
+  private $host = 'localhost';
+  private $user = "root";
+  private $pwd = "";
+  private $dbName = "ClassSchedulingSystem";
 
   // $herokuHost = mysql -u b9e547a06bfec3 -h us-cdbr-east-02.cleardb.com -p heroku_47a94c0a6cb5111 < classschedulingsystem.sql
 
